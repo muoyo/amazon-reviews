377e46927fc52282a6927deab495a51bee34e933
Muoyo Okome <mokome@gmail.com>
2020-02-05 02:00:17 -0500

    Streamline

--- python_files/classification.py
+++ python_files/classification.py
@@ -13,13 +13,15 @@ from sklearn.model_selection import train_test_split
 
 def get_train_test_split(df, test_size=.25):
 
-    X = df.loc[:, ['helpful_votes', 'total_votes', 'neg', 'neu', 'pos', 'compound']]
+    X = df.loc[:, ['neg', 'neu', 'pos', 'compound', 'review_fulltext']]
     y = df.loc[:, 'star_rating']
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    return train_test_split(X, y, test_size=test_size)
     
-    smote = SMOTE()
-    
-    X_train, y_train = smote.fit_sample(X_train, y_train) 
-    
-    return X_train, X_test, y_train, y_test
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+    # smote = SMOTE()
+
+    # X_train, y_train = smote.fit_sample(X_train, y_train) 
+
+    # return X_train, X_test, y_train, y_test
