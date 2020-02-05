083b19833c41e80f6d46a9a4e5491eea74391b56
Muoyo Okome <mokome@gmail.com>
2020-02-04 23:36:54 -0500

    Confusion Matrices

--- python_files/visualizations.py
+++ python_files/visualizations.py
@@ -9,14 +9,22 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+from sklearn.metrics import plot_confusion_matrix
+
 
 # Controls appearance of seaborn plots. Options: paper, notebook, talk, or poster
 SEABORN_CONTEXT = 'notebook' 
 SEABORN_PALETTE = sns.color_palette("bright")
 sns.set_context(SEABORN_CONTEXT)
 
+local_stopwords = ['watch', 'watches', 'watche', 'br', 'five', 'star', 'good', 'old', 'year', 'way', 'say', 
+                   'great', 'happy', 'nice', 'stars', 'love', 'used', 'problem', 'second', 'highly', 'recommend',
+                   'time', 'one', 'two', 'three', 'four', 'look', 'band', 'Amazon']
+
 def wordcloud(wordstring, stopwords=False, figsize=(20,20)):
     sw = set(STOPWORDS)
+    sw.update(local_stopwords)
+    
     if stopwords: sw.update(stopwords)
     
     # Create and generate a word cloud image:
@@ -25,10 +33,23 @@ def wordcloud(wordstring, stopwords=False, figsize=(20,20)):
     # Display the generated image:
     plt.figure(figsize=figsize)
     plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
+    plt.axis('off')
     plt.show()
 
 
+def n_star_review_wordcloud (df, n, stopwords=False, figsize=(20,20)):
+    n_star_reviews = df.loc[df['star_rating'] == n]['review_fulltext']
+    n_star_reviews = n_star_reviews.str.cat(sep=' ')
+    
+    wordcloud(n_star_reviews, stopwords, figsize)
+
+
+def show_confusion_matrix(classifier, X, y, title='', figsize=(10,10), cmap=plt.cm.Blues, normalize='true'):
+    f, ax = plt.subplots(figsize=figsize)
+    plot_confusion_matrix(classifier, X, y, cmap=cmap, ax=ax, normalize=normalize)
+    ax.set_title(title);
+    
+    
 def barplot(group_series, title='', xlabel='', ylabel='', figsize=(15,10), color=SEABORN_PALETTE[2]):
 
     
